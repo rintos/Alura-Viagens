f5549c3bb7b127d4b4b84d6fd14b77438435ca9c
VictorSantoryu <victor.santoryu@github.com>
2018-10-16 00:30:08 -0300

    Criando Projeto Alua Viagens Listagem de conteúdo no tableview

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    
 
+    let listaViagens:Array<String> = ["Sao Paulo", "Ceara", "Rio de Janeiro"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tabelaViagens.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
