3155c6d57b2f99b98662e69515c51a0d9237aea8
VictorSantoryu <victor.santoryu@github.com>
2018-10-23 19:06:33 -0300

    Finalizado ajuste com StackView

--- Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class PacotesViagensViewController: UIViewController,UICollectionViewDataSource {
+class PacotesViagensViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
 
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         colecaoPacotesViagem.dataSource = self
+        colecaoPacotesViagem.delegate = self
         // Do any additional setup after loading the view.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return self.listaViagens.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
-        celulaPacote.backgroundColor = UIColor.blue
+       // celulaPacote.backgroundColor = UIColor.blue
+        let viagemAtual = listaViagens[indexPath.item]
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = viagemAtual.preco
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+       celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 8
+        
         return celulaPacote
     }
     
@@ -35,14 +48,19 @@ class PacotesViagensViewController: UIViewController,UICollectionViewDataSource 
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCollection = collectionView.bounds.width / 2
+        return CGSize(width: larguraCollection - 15, height: 175)
     }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
